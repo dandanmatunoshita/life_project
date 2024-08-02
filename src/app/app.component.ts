e077103bf4d3385ae2abd184727cab676d1c6f53
gabrsilvab <lexdex100@gmail.com>
2024-08-01 23:40:51 -0300

    1.13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
 // src/app/app.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { environment } from 'src/environments/environment.development';
 
+const GoogleGenAI = new GoogleGenerativeAI(environment.API_KEY);
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "application/json",
+}
+
+const model = GoogleGenAI.getGenerativeModel({
+  model: 'gemini-1.5-flash',
+  ...generationConfig
+})
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent  implements OnInit
+{
   title(title: any) {
     throw new Error('Method not implemented.');
   }
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
-  // Outros métodos e lógica do componente, se necessário
+
+ 
 }
